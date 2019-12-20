19c02602aeb812a9022508181fea81939ab31331
doren <56702391+doranium2013@users.noreply.github.com>
2019-12-20 08:52:42 -0800

    Got characters and data to show up

--- src/components/CharacterCard.js
+++ src/components/CharacterCard.js
@@ -1,6 +1,15 @@
 import React from "react";
+import CharacterList from "./CharacterList"
 
-export default function CharacterCard() {
-  return <span>todo: character</span>;
-}
-
+export default function CharacterCard({
+  char
+}) { 
+  const { name, image, gender, species } = char;
+  return ( 
+    <div>
+    <img src = {image}></img>
+    <h1>{name}</h1>
+    <h2>{gender}</h2>
+    <h3>{species}</h3>
+    </div>
+  )}
