ac07eeb7d249dfd8c8c26af8e7363e73993057ec
doren <56702391+doranium2013@users.noreply.github.com>
2019-12-20 08:58:57 -0800

    added styles to align items

--- src/components/CharacterCard.js
+++ src/components/CharacterCard.js
@@ -1,15 +1,16 @@
 import React from "react";
-import CharacterList from "./CharacterList"
+
 
 export default function CharacterCard({
   char
-}) { 
+}) {
   const { name, image, gender, species } = char;
-  return ( 
+  return (
     <div>
-    <img src = {image}></img>
-    <h1>{name}</h1>
-    <h2>{gender}</h2>
-    <h3>{species}</h3>
+      <img src={image}></img>
+      <h1>{name}</h1>
+      <h2>Gender: {gender}</h2>
+      <h3>Species: {species}</h3>
     </div>
-  )}
+  )
+}
