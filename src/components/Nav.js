e0faa58d41ba01bc86e62ce3087d3ff95dad55eb
doren <56702391+doranium2013@users.noreply.github.com>
2019-12-20 11:15:45 -0800

    added some styling components

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,18 +1,38 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import styled from "styled-components"
+
+const Navigation =styled.div`
+display:flex;
+justify-content: space-around;
+align-items:center;
+background: lightblue;
+color: white;
+
+`
+
+const Text =styled.div`
+font-size: 25px;
+font-family: 'Odibee Sans', cursive;
+
+
+
+`
 
 const Nav = () => {
     return (
       
-        <div className="Nav">
-          <div>
+        <Navigation>
+          <Text>
             <Link to="/">Home</Link>
-          </div>
-          <div>
+          </Text>
+          <Text>
               <Link to ="characters">Characters</Link>
-          </div>
+          </Text>
+          <Text>
           <Link to ="search">Search</Link>
-          </div>
+          </Text>
+          </Navigation>
          
     
     );
--- src/components/SearchForm.js
+++ src/components/SearchForm.js
@@ -5,26 +5,26 @@ export default function SearchForm() {
   const [search, setSearch] = useState("");
   const [results, setResults] = useState([]);
 
-  function handleChange(e){
+  function handleChange(e) {
     e.preventDefault();
     setSearch(e.target.value);
     console.log(e.target.value);
   }
- 
 
-useEffect(() => {
-  axios
-  .get("https://rickandmortyapi.com/api/character/")
-  .then(res => {
-    console.log(res.data.results);
-    let characters = res.data.results.filter(char => char.name.toLowerCase().includes(search.toLowerCase())
-    );
-    setResults(characters);
-    console.log(results);
-  })
-  .catch(err => console.log(err));
-  
-}, [search])
+
+  useEffect(() => {
+    axios
+      .get("https://rickandmortyapi.com/api/character/")
+      .then(res => {
+        console.log(res.data.results);
+        let characters = res.data.results.filter(char => char.name.toLowerCase().includes(search.toLowerCase())
+        );
+        setResults(characters);
+        console.log(results);
+      })
+      .catch(err => console.log(err));
+
+  }, [search])
 
 
   return (
@@ -33,23 +33,29 @@ useEffect(() => {
         <label htmlFor="search">
           Search Characters
         </label>
-        <input 
-        className="search-bar"
-        name="search"
-        type="text"
-        id="search"
-        placeholder="Search Characters"
-        value={search}
-        onChange={handleChange}
-        autoComplete="off"/>
+        <input
+          className="search-bar"
+          name="search"
+          type="text"
+          id="search"
+          placeholder="Search Characters"
+          value={search}
+          onChange={handleChange}
+          autoComplete="off" />
         <button type="submit">Submit</button>
       </form>
       <div>
         {results.map(result => {
-          return <div>Name: {result.name}</div>;
+          return <div>
+            <img src={result.image}></img>
+            <h1>{result.name}</h1>
+            <h2>Gender: {result.gender}</h2>
+            <h3>Species: {result.species}</h3>
+          </div>;
         })}
       </div>
-    
+
     </section>
   );
 }
+
